3cfcfa4d1fee5877188e16baef21bdd32b69edf2
saralins <saralinstech@gmail.com>
2026-02-02 04:46:18 -0300

    Refactor Prisma configuration to streamline imports and enhance environment variable usage for database connection

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'prisma/config';
+// import { defineConfig } from 'prisma/config';
+
+// export default defineConfig({
+//   schema: 'prisma/schema.prisma',
+
+//   datasource: {
+//     url: process.env.DATABASE_URL!,
+//   },
+
+//   migrations: {
+//     path: 'prisma/migrations',
+//   },
+// });
+
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
-
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-
+  schema: "prisma/schema.prisma",      // caminho pro seu schema
   migrations: {
-    path: 'prisma/migrations',
+    path: "prisma/migrations",         // onde ficam as migrations
+  },
+  datasource: {
+    url: env("DATABASE_URL"),          // ← A URL de conexão fica aqui
   },
 });
